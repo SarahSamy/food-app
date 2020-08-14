0272abb5cdbaefeac21684d540c52ccb46aa0ff6
SarahSamy <sarah.refaat92@gmail.com>
2020-08-14 12:34:57 +0200

    Change app name to food app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyKitchenApp
+# Food App
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-kitchen-app'`, () => {
+  it(`should have as title 'food-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-kitchen-app');
+    expect(app.title).toEqual('food-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-kitchen-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to food-app!');
   });
 });
