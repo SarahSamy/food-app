9557a773a7206283efd22797904585806a64b510
SarahSamy <sarah.refaat92@gmail.com>
2021-11-15 20:12:07 +0400

    refactor: move pages dir to top level

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './features/pages/home/home.component';
-import { PageNotFoundComponent } from './features/pages/page-not-found/page-not-found.component';
-import { RecipesComponent } from './features/pages/recipes/recipes-listing-by-filter/recipes.component';
-import { RecipeDetailsPageComponent } from './features/pages/recipes/recipe-details/recipe-details-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { RecipesComponent } from './pages/recipes/recipes-listing-by-filter/recipes.component';
+import { RecipeDetailsPageComponent } from './pages/recipes/recipe-details/recipe-details-page.component';
 import { RecipeSearchComponent } from './features/recipes/recipe-search/recipe-search.component';
-import { RecipesSearchResultComponent } from './features/pages/recipes/recipes-search-result/recipes-search-result.component';
+import { RecipesSearchResultComponent } from './pages/recipes/recipes-search-result/recipes-search-result.component';
 import Recipe from './model/recipe'
 let recipes: Recipe[] = [];
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,19 @@ import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './core/header/main-header/main-header.component';
 import { PrimaryButtonComponent } from './features/inputs/primary-button/primary-button.component';
 import { BasicSliderComponent } from './shared/basic-slider/basic-slider.component';
-import { HomeComponent } from './features/pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { MovingInPathComponent } from './features/animation/moving-in-path/moving-in-path.component';
 import { RecipesListingComponent } from './features/recipes/recipes-listing/recipes-listing.component';
 import { RecipeItemComponent } from './features/recipes/recipes-item/recipe-item.component';
-import { PageNotFoundComponent } from './features/pages/page-not-found/page-not-found.component';
-import { RecipesComponent } from './features/pages/recipes/recipes-listing-by-filter/recipes.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { RecipesComponent } from './pages/recipes/recipes-listing-by-filter/recipes.component';
 import { RecipesListingFilterComponent } from './features/recipes/recipes-listing-filter/recipes-listing-filter.component';
-import { RecipeDetailsPageComponent } from './features/pages/recipes/recipe-details/recipe-details-page.component';
+import { RecipeDetailsPageComponent } from './pages/recipes/recipe-details/recipe-details-page.component';
 import { RecipeDetailsComponent } from './features/recipes/recipe-details/recipe-details.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { SignUpComponent } from './features/sign-up/sign-up.component';
 import { RecipeSearchComponent } from './features/recipes/recipe-search/recipe-search.component';
-import { RecipesSearchResultComponent } from './features/pages/recipes/recipes-search-result/recipes-search-result.component';
+import { RecipesSearchResultComponent } from './pages/recipes/recipes-search-result/recipes-search-result.component';
 
 @NgModule({
   declarations: [
